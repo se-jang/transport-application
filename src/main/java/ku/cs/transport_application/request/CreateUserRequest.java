85ca0973b771931458cb1d3c7c4b962385a27799
JIMpk2w <pakawat.pan@ku.th>
2024-10-07 20:03:45 +0700

    make validation for data cont.

--- src/main/java/ku/cs/transport_application/request/CreateUserRequest.java
+++ src/main/java/ku/cs/transport_application/request/CreateUserRequest.java
@@ -11,11 +11,21 @@ public class CreateUserRequest {
     @Size(min=4, message = "Username must have at least 4 characters")
     private String username;
 
+    @NotBlank(message = "Name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name only contain letters")
+    private String name;
+
     @NotBlank
     @Size(min=8, max=128, message = "Password must have at least 8 characters")
     private String password;
 
-    @NotBlank(message = "Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name only contain letters")
-    private String name;
+    @Pattern(regexp = "^[0-9]*$", message = "Phone number must be numeric")
+    @Size(min = 10, max = 10, message = "Phone number must be exactly 10 digits")
+    private String phoneNum;
+
+    @NotBlank(message = "User type is required")
+    private String userType;
+
+    @NotBlank(message = "Address is required")
+    private String address;
 }
