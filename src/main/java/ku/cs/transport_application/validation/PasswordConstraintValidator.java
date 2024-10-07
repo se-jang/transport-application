1e050a68780182d186387d16cdb0b2b53228fc7a
Pariyanuch <pariyanuch.m@ku.th>
2024-10-07 22:19:04 +0700

    fixed password validation & create componentConfig

--- src/main/java/ku/cs/transport_application/validation/PasswordConstraintValidator.java
+++ src/main/java/ku/cs/transport_application/validation/PasswordConstraintValidator.java
@@ -15,10 +15,9 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
         boolean hasUppercase = Pattern.compile("[A-Z]").matcher(password).find();
         boolean hasLowercase = Pattern.compile("[a-z]").matcher(password).find();
         boolean hasDigit = Pattern.compile("[0-9]").matcher(password).find();
-        boolean hasSpecialChar = Pattern.compile("[^a-zA-Z0-9]").matcher(password).find();
         boolean isLengthValid = password.length() >= 8 && password.length() <= 128;
 
-        if (hasUppercase && hasLowercase && hasDigit && hasSpecialChar && isLengthValid) {
+        if (hasUppercase && hasLowercase && hasDigit && isLengthValid) {
             return true;
         }
 
@@ -26,7 +25,6 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
         if (!hasUppercase) violationMessage.append("at least one uppercase letter, ");
         if (!hasLowercase) violationMessage.append("at least one lowercase letter, ");
         if (!hasDigit) violationMessage.append("at least one digit, ");
-        if (!hasSpecialChar) violationMessage.append("at least one special character, ");
         violationMessage.append("and must be between 8 and 128 characters long.");
 
         context.buildConstraintViolationWithTemplate(violationMessage.toString())
