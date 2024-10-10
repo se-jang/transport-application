a9085fb6ea70fa21280cb05c0c4cfea787e6fba2
JIMpk2w <pakawat.pan@ku.th>
2024-10-10 18:58:32 +0700

    edit index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
 
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+  },
+];
 const router = createRouter({
-  history: createWebHistory("/"),
-  routes: [
-    {
-      path: "/login",
-      name: "Login",
-      component: LoginView,
-    },
-  ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const routeExists = routes.some(route => route.name === to.name);
+
+  if (!routeExists) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
+});
+
 
 export default router;
