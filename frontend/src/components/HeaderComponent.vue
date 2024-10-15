926ba89d133251f93bcfedfc875f0f9ecc41c361
JIMpk2w <pakawat.pan@ku.th>
2024-10-15 23:08:18 +0700

    create customer header

---
+++ frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HeaderAdmin v-if="role === 'admin'" />
+    <HeaderCustomer v-else-if="role === 'customer'" />
+    <HeaderCompany v-else-if="role === 'company'" />
+    <HeaderWorker v-else-if="role === 'worker'" />
+  </div>
+</template>
+
+<script>
+import HeaderAdmin from '..HeaderAdmin.vue';
+import HeaderCustomer from "..HeaderCustomer.vue";
+import HeaderCompany from "..HeaderCompany.vue";
+import HeaderWorker from "..HeaderWorker.vue";
+
+export default {
+  props: ["role"],
+  components: {
+    HeaderAdmin,
+    HeaderCustomer,
+    HeaderCompany,
+    HeaderWorker,
+  },
+};
+</script>
--- frontend/src/components/HeaderCustomer.vue
+++ frontend/src/components/HeaderCustomer.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="company-header">
-    <h1>Company</h1>
+  <div class="customer-header">
+    <h1>Customer</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HeaderCompany',
+  name: 'HeaderCustomer',
 };
 </script>
 
 <style scoped>
-.company-header {
+.customer-header {
   background-color: #f8f8f8;
   padding: 20px;
   text-align: center;
