f7a509eed696e464ee7f0351ec63af59ab6c5e10
JIMpk2w <pakawat.pan@ku.th>
2024-10-16 19:09:01 +0700

    craete order company card

---
+++ frontend/src/components/OrderCompanyCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="order-card">
+    <h2 class="order-id">Order ID</h2>
+    <p class="due-date">Due Date: ...</p>
+    <button class="details-button">Details</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderCompanyCard",
+};
+</script>
+
+<style scoped>
+.order-card {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.order-id,
+.due-date {
+  font-family: "Inter", sans-serif;
+  margin: 0;
+}
+
+.details-button {
+  padding: 10px 20px;
+  font-family: "Inter", sans-serif;
+  background-color: gray;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
