e0701aa79f435270a213d4b1d907d0f1f78f25af
JIMpk2w <pakawat.pan@ku.th>
2024-10-26 20:57:30 +0700

    add username in jwt

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -3,18 +3,26 @@ import { createStore } from 'vuex';
 export default createStore({
     state: {
         userRole: "",
+        username: "",
     },
     getters: {
         userRole: (state) => state.userRole,
+        username: (state) => state.username,
     },
     mutations: {
         setUserRole(state, role) {
             state.userRole = role;
         },
+        setUsername(state, username) {
+            state.username = username;
+        },
     },
     actions: {
         updateUserRole({ commit }, role) {
             commit("setUserRole", role);
+        },
+        updateUsername({ commit }, username) {
+            commit("setUsername", username);
         },
     },
 });
