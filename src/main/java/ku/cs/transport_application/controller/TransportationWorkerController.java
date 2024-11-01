f5157b3d82828edbc272796e3cecf6444d39ca04
JIMpk2w <pakawat.pan@ku.th>
2024-11-01 18:20:16 +0700

    change path

--- src/main/java/ku/cs/transport_application/controller/TransportationWorkerController.java
+++ src/main/java/ku/cs/transport_application/controller/TransportationWorkerController.java
@@ -5,10 +5,12 @@ import ku.cs.transport_application.DTO.TransportationWorkerDTO;
 import ku.cs.transport_application.service.OrderService;
 import ku.cs.transport_application.service.TransportationWorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -30,6 +32,16 @@ public class TransportationWorkerController {
     @GetMapping("/{workerId}")
     public ResponseEntity<?> getTransportationWorkerDetails(@PathVariable UUID workerId) {
         return ResponseEntity.ok(orderService.getOrdersByWorker(workerId));
+    }
+
+    @PostMapping("/worker/worker-detail/{workerId}/add-order")
+    public ResponseEntity<?> assignWorkerToOrder(@RequestParam("orderId") UUID orderId, @PathVariable("workerId") UUID workerId) {
+        try {
+            orderService.upDateOrderToWorker(workerId, orderId);
+            return new ResponseEntity<>(Map.of("message", "Worker assigned to order successfully"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("error", "Failed to assign worker to order"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
