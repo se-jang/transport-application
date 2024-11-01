60344571bd3bf31181adfaa8b171e508ddaee309
PariyanuchM <pariyanuch.m@ku.th>
2024-11-01 18:14:50 +0700

    fix file service

--- src/main/java/ku/cs/transport_application/service/FileService.java
+++ src/main/java/ku/cs/transport_application/service/FileService.java
@@ -4,6 +4,7 @@ import ku.cs.transport_application.entity.Order;
 import ku.cs.transport_application.repository.OrderRepository;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -15,6 +16,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
+@Service
 public class FileService {
     OrderRepository orderRepository;
 
